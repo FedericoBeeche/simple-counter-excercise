6199824ed343c0106772a218a7760f759454d2fc
FedericoBeeche <fbeeche@gmail.com>
2021-03-22 05:00:14 +0000

    finished counter

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,22 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
+export function SecondCounter(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<i className="iconoReloj fa fa-clock"></i>
+			<div className="digitoCuatro">{props.numeroCuatro % 10}</div>
+			<div className="digitoTres">{props.numeroTres % 10}</div>
+			<div className="digitoDos">{props.numeroDos % 10}</div>
+			<div className="digitoUno">{props.numeroUno % 10}</div>
 		</div>
 	);
 }
+
+SecondCounter.propTypes = {
+	numeroCuatro: PropTypes.number,
+	numeroTres: PropTypes.number,
+	numeroDos: PropTypes.number,
+	numeroUno: PropTypes.number
+};
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,25 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { SecondCounter } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function() {
+	var numCuatro = Math.floor(counter / 10000);
+	var numTres = Math.floor(counter / 1000);
+	var numDos = Math.floor(counter / 100);
+	var numUno = Math.floor(counter / 10);
+
+	counter++;
+	console.log(counter);
+	ReactDOM.render(
+		<SecondCounter
+			numeroUno={numUno}
+			numeroDos={numDos}
+			numeroTres={numTres}
+			numeroCuatro={numCuatro}
+		/>,
+		document.querySelector("#app")
+	);
+}, 5);
